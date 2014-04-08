cd4d6bec067d3a24f6eb8c512b51e5fb09a58670
naoto <naoto0101@gmail.com>
2014-04-08 13:47:39 +0900

    commit init

---
+++ MoneyLib/lib/src/main/java/jp/co/misyobun/moneylib/lib/Money.java
@@ -0,0 +1,16 @@
+package jp.co.misyobun.moneylib.lib;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by takahashinaoto on 2014/04/08.
+ */
+public class Money {
+
+    public static List<String> getMoneies() {
+        List<String> moneies = new ArrayList<String>();
+        moneies.add("1000円");
+        return moneies;
+    }
+}
